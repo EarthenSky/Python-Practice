70cd79e000516765203c842e8b7a9f5aa3017ca6
EarthenSky <gabegames27@gmail.com>
2018-02-13 15:26:29 -0800

    started work on the task sequencer

--- cpu_task_sequencer/StangGabe_ch3-4_taskSequencer.py
+++ cpu_task_sequencer/StangGabe_ch3-4_taskSequencer.py
@@ -1,1 +1,14 @@
-pass
+# subprocess is used instead of popen because it is a newer verison
+# This module lets the program run processes
+import subprocess
+# This module lets the program use multiple cores
+import multiprocessing
+
+print "Task Sequencer"
+in_val = raw_input("Press enter to continue...")
+
+# This function runs
+def run_task():
+    subprocess.call("stangGabe_stochasticProcess.py", shell=True)
+
+run_task()
--- cpu_task_sequencer/stangGabe_stochasticProcess.py
+++ cpu_task_sequencer/stangGabe_stochasticProcess.py
@@ -1,3 +1,27 @@
-import sys  # For outputting values
+import sys  # Imported for outputting values
+from threading import Timer  # Imported for using timers
+
+# This is the flag that keeps the math loop ruinning
+g_do_math = True
+
+# This method ends the program and gives an output value
+def end_task():
+    global g_do_math
+    g_do_math = False  # Stop the math loop
+
+def start_task_timer():
+    timer = Timer(1.0, end_task)
+    timer.start()
+
+# Start a timer that automatically closes the task when done
+start_task_timer()
+
+# This is the counter that is incremented to use cpu
+inc_value = 0
+
+# This is the cpu hog loop
+while g_do_math:
+    inc_value += 1
+    print inc_value
 
 sys.exit(100)  # Temp output value
