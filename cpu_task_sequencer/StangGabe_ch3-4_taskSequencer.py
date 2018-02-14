8bec39c2b4256e5a531b1979207a9ff949582898
EarthenSky <gabegames27@gmail.com>
2018-02-14 15:36:16 -0800

    tyring to figure out multiple processes

--- cpu_task_sequencer/StangGabe_ch3-4_taskSequencer.py
+++ cpu_task_sequencer/StangGabe_ch3-4_taskSequencer.py
@@ -1,14 +1,83 @@
-# subprocess is used instead of popen because it is a newer verison
-# This module lets the program run processes
+'''# Subprocess is used instead of popen because it is a newer verison.
+# This module lets the program run processes.
 import subprocess
-# This module lets the program use multiple cores
+# This module lets the program use multiple cores.
 import multiprocessing
 
 print "Task Sequencer"
 in_val = raw_input("Press enter to continue...")
 
-# This function runs
+# This function runs a task
 def run_task():
     subprocess.call("stangGabe_stochasticProcess.py", shell=True)
 
-run_task()
+#run_task();
+#run_task();
+run_task();
+
+# Instantiate process object
+if __na12me__ == '__main__':
+    p = multiprocessing.Process(target=run_task, args=())
+    p.start()
+    p.join()
+    '''
+
+##
+
+'''
+import subprocess
+from multiprocessing import Process
+import os
+
+def info(title):
+    print title
+    print 'module name:', __name__
+    if hasattr(os, 'getppid'):  # only available on Unix
+        print 'parent process:', os.getppid()
+    print 'process id:', os.getpid()
+
+def f(name):
+    info('function f')
+    print 'hello', name
+    subprocess.call("stangGabe_stochasticProcess.py", shell=True)
+
+if __name__ == '__main__':
+    info('main line')
+    p = Process(target=f, args=('bob',))
+    p.start()
+    p.join()
+    '''
+
+from multiprocessing import Process
+import sys
+import os
+
+# This function runs a task
+def run_task():
+    #subprocess.call("stangGabe_stochasticProcess.py", shell=True)
+    print "1"
+    print "2"
+    print "3"
+    print "4"
+    print "5"
+
+processes = []
+
+if __name__ == '__main__':
+    p = Process(target=run_task, args=())
+    p.start()
+    processes.append(p)
+    p.join()
+
+for m in range(1, 3):
+    pass
+
+for p in processes:
+    #p.join()
+    pass
+
+
+
+#What does p.join() do?
+#async allowed
+#
--- cpu_task_sequencer/stangGabe_stochasticProcess.py
+++ cpu_task_sequencer/stangGabe_stochasticProcess.py
@@ -1,5 +1,6 @@
 import sys  # Imported for outputting values
 from threading import Timer  # Imported for using timers
+import time
 
 # This is the flag that keeps the math loop ruinning
 g_do_math = True
@@ -10,7 +11,7 @@ def end_task():
     g_do_math = False  # Stop the math loop
 
 def start_task_timer():
-    timer = Timer(1.0, end_task)
+    timer = Timer(5.0, end_task)
     timer.start()
 
 # Start a timer that automatically closes the task when done
@@ -22,6 +23,9 @@ inc_value = 0
 # This is the cpu hog loop
 while g_do_math:
     inc_value += 1
-    print inc_value
+    time.sleep(0.2)
+    #print inc_value
+
+#time.sleep(2)
 
 sys.exit(100)  # Temp output value
