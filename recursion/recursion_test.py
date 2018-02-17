524ba67071d08d0eea196f6c21ba9464b2c77ce5
EarthenSky <gabegames27@gmail.com>
2018-02-17 15:09:28 -0800

    moved some files and practiced recursion

--- recursion/recursion_test.py
+++ recursion/recursion_test.py
@@ -2,7 +2,6 @@ import sys
 
 # This function finds the sum of a fibonacci sequence based on its length
 def fib_sum(length):
-
     # Base cases
     if length == 0:
         sys.stdout.write('0 ')
@@ -17,4 +16,15 @@ def fib_sum(length):
     # Equation
     return 2 * fib_sum(length - 1) - fib_sum(length - 3)
 
-print fib_sum(10)
+print( fib_sum(10) )
+
+# This function recursively reverses a string.
+def reverse_string(string):
+    # Base case
+    if len(string) <= 1:
+        return string[0]
+
+    # Equation
+    return string[len(string) - 1] + reverse_string(string[:-1])
+
+print( reverse_string("hello this is a car.\n By the way it is also a very very nice car.") )
