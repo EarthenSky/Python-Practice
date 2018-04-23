26e59dd99c5f1025d639e7f7a9c1c21ca3311405
EarthenSky <gabegames27@gmail.com>
2018-04-23 08:35:48 -0700

    -_-

--- python_challenge/lvl8.py
+++ python_challenge/lvl8.py
@@ -1,13 +1,18 @@
-g_un = 'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
-g_p = 'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+g_un = b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+g_p = b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
 
-print(g_un)
-print(g_p)
+#print('\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00'.decode('iso-8859-1'))
+#print(g_p.decode('iso-8859-1'))
+#print("\xe2\x82\x80")
 
-print("\xe2\x82\x80")
+#file_object = open("file.txt", "w")
+#file_object.write(g_un + "|||" + g_p)
+import zlib, gzip
 
-file_object = open("file.txt", "w")
-file_object.write(g_un + "|||" + g_p)
+y = gzip.compress(b'0sfg^sdfgcoetohDabyss.')
+print( y.split(b" ") )
+#x = gzip.decompress(g_p)
+#y = zlib.decompress(g_p)
 
 #import pyperclip
 #pyperclip.copy('\x00')
