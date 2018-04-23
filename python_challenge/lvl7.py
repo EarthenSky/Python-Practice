541912d0614996bbaaa32a826222d37dfeb29082
EarthenSky <gabegames27@gmail.com>
2018-04-22 22:54:16 -0700

    stuck again... lvl 8..

--- python_challenge/lvl7.py
+++ python_challenge/lvl7.py
@@ -4,11 +4,19 @@ img = PIL.Image.open('oxygen.png')
 rgb_img = img.convert('RGB')
 
 # Makes a list of all of the rgb greyscale values of the pixels
-out_list = []
+out_list = ""
 for i in range(0, 87):
     r, g, b = rgb_img.getpixel((i * 7, 48))
-    out_list.append(chr(r))
+    out_list += chr(r)
 
 print( str(out_list) )
+
+_next = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+
+out = ""
+for num in _next:
+    out += chr(num)
+
+print(out)
 
 #----------------------------------
