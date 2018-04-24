f0d4099c0f55b4a3604ad9ad310df5eecbcc3363
EarthenSky <gabegames27@gmail.com>
2018-04-24 08:22:41 -0700

    got lvl11. I actually did it by accident and it scared me.

--- python_challenge/lvl11.py
+++ python_challenge/lvl11.py
@@ -4,22 +4,44 @@ img = PIL.Image.open('cave.jpg')
 width, height = img.size
 
 out1 = img.convert('RGB')
-out2 = img.convert('RGB')
+#out2 = img.convert('RGB')
 
 pixels1 = out1.load()
-pixels2 = out2.load()
+#pixels2 = out2.load()
 
 save_px = (0, 0, 0)
 
-for i in range(width):
-    for j in range(0, height, 2):
-        save_px = rgb_img.getpixel()
-        pixels1[i, j] = (0, 0, 0) #Set the colour.
-        #if (i + j) % 2 == 0:
+_img_list = []
+_img_list2 = []
 
-        #else:
-            pixels2[i, j] = (0, 0, 0) #Set the colour.
+#Separate Imgs
+count = 0
+which = False
+for y in range(height):
+    for x in range(0, width, 1):
+        save_px = out1.getpixel( (x, y) )
+        pixels1[x, y] = save_px #Set the colour.
+        if ((x + (y*count)) % 2) == 0:
+
+            which = not which
+            if which == True:
+                _img_list.append(save_px)
+            else:
+                _img_list.append(save_px)
+
+    count += 1
+
+img2 = PIL.Image.new(img.mode, (int(width), int(height)))
+img2.putdata(_img_list)
 
 #-- Show images
-out1.show()
-out2.show()
+#img2.show()
+
+#img3 = PIL.Image.new(img.mode, (int(width/2), int(height/2)))
+#img3.putdata(_img_list2)
+
+#-- Show images
+#img3.show()
+#out2.show()
+
+img2.save('img2.png')
